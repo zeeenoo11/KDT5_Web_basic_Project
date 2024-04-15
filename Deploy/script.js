62e34d371ae94792f59834c30a31f1acd7fa8bee
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2024-04-15 13:28:04 +0900

    add pikachu animation😊

--- Deploy/script.js
+++ Deploy/script.js
@@ -2,3 +2,12 @@ function changePage(pageName) {
   var iframe = document.getElementById("contentFrame");
   iframe.src = pageName;
 }
+function changeImage() {
+  const pikachu = document.getElementById("pikachu");
+  pikachu.src = "pikachu2.png"; // 마우스를 올렸을 때 바뀔 이미지의 경로
+}
+
+function restoreImage() {
+  const pikachu = document.getElementById("pikachu");
+  pikachu.src = "pikachu.png"; // 마우스를 내렸을 때 다시 복원될 이미지의 경로
+}
